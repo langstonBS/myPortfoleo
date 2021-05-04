eac7e30999377825f2a43db9b8e712a1475c7bf5
langstonBS <langston.beckwithstanley@gmail.com>
2021-05-03 20:00:53 -0700

    alot of changes

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,19 +1,65 @@
 import React from 'react'
+import './Home.scss'
+import ScrollAnimation from 'react-animate-on-scroll';
+
+
 const Home = () => {
     return (
-        <>
-            <p>my home page</p>
-            <h1>who are you</h1>
-            <img src="https://placekitten.com/g/200/300" />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <address>
-                <h5>email</h5>
-                <h5>Github</h5>
-                <h5>liniked in </h5>
+        <div className="outerGrid">
 
-            </address>
+            <section className="introduction">
+                <h1>Welcom To Langstons Land</h1>
+                <p>A portfoleo Deticated to Langston Becwith-Stanley and all things realated to Langston and Langstons Coding </p>
+            </section>
 
-        </>
+
+
+            <section className="knownSkills">
+                <h2> Known Skills</h2>
+                <ul className="listClass">
+                    <li>
+                    </li>
+                    <li>
+                        node.js
+                    </li>
+                    <li>
+                        MongoDB
+                    </li>
+                    <li>
+                        SQL
+                    </li>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        css
+                    </li>
+                    <li>
+                        Postgress
+                    </li>
+                </ul>
+            </section>
+
+            <section className="learningSkills">
+                <h2> Learning</h2>
+                <ul>
+                    <li>
+                        React Native
+                    </li>
+                    <li>
+                        Styling (css, css3 and scss sass)
+                    </li>
+                </ul>
+            </section>
+            <section className="aboutMe">
+                <h2>Who Am I</h2>
+                <p>Software Engineer who thrives in collaborative and dynamic teams who values great communication,
+                makes sure everyone's voice is heard, and brings great empathy to any environment. Loves slaying
+                code in React and able to bring remote cohesiveness to any dev environment.
+                </p>
+            </section>
+
+        </div>
     )
 }
 
---
+++ src/StylesAndTemps/PageStyle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageStyle() {
+    return (
+        <div>
+
+        </div>
+    )
+}
