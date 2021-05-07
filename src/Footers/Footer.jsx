870b737765a3eba5d7a77e2d9d4822c4589481fb
langstonBS <langston.beckwithstanley@gmail.com>
2021-05-07 10:09:13 -0700

    footer links and medea q added

--- src/Footers/Footer.jsx
+++ src/Footers/Footer.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import './Footer.css'
+import './Footer.scss'
 
 export default function Footer() {
     return (
-        <>
-            <footer className="outFoot">
-                <address className="footContaner">
-                    <h6>Langston Beckwith-Stanley</h6>
-                    <p>Github - LinkedIn</p>
-                </address>
-            </footer>
-        </>
-
+        <footer className="outFoot">
+            <address className="footContaner">
+                <h6 className="footerNameText">Langston Beckwith-Stanley</h6>
+                <p className="footerLinks">
+                    <a href="https://github.com/langstonBS" target="_blank"> Github </a> -
+                    <a href="https://www.linkedin.com/in/langston-beckwith-stanley/" target="_blank"> LinkedIn </a>
+                </p>
+            </address>
+        </footer>
     )
 }
