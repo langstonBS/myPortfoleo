b02a1193b2a857ca9a15c8dcaca6b9c2c980c246
langstonBS <langston.beckwithstanley@gmail.com>
2021-05-13 16:33:51 -0700

    websight update

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -43,10 +43,16 @@ const Home = () => {
                 <h2> Learning</h2>
                 <ul>
                     <li>
+                        Rust
+                    </li>
+                    <li>
                         React Native
                     </li>
                     <li>
                         Styling (css, css3 and scss sass)
+                    </li>
+                    <li>
+                        Graph QL
                     </li>
                 </ul>
             </section>
@@ -54,9 +60,7 @@ const Home = () => {
                 <h2>Who Am I</h2>
                 <p>I have a coding philosophy that values communication and community to solve complex problems. Having just finished being a teaching assistant for a coding school, I have learned how to cultivate an environment that empowers all voices in the (Zoom) room, I also saw firsthand how success can only be attained by open communication.</p>
                 <p>I'm currently working on a project that is geared towards the disabled community.  to best serve the community we must use semantic HTML, follow the design from a design team that is based on interviews from the disabled community.  Use the most up-to-date react practices and the latest testing libraries to ensure that our website is fully accessible for everyone.
-                Using these skills along with new ones I am sure to learn through my life, I want to bring the same focus on community by using open communication and Accessibility driven development to future projects.
-
-                </p>
+                Using these skills along with new ones I am sure to learn through my life, I want to bring the same focus on community by using open communication and Accessibility driven development to future projects.</p>
             </section>
 
         </div>
