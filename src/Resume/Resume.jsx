e9e14a49c11910e20d6f1aae206775f8ff8ea583
langstonBS <langston.beckwithstanley@gmail.com>
2021-09-30 19:16:36 -0700

    changing computers

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import Navagation from '../Navagation/Navagation'
+import './Resume.css'
+
 
 export default function Resume() {
     return (
-        <div>
-            Resume
-        </div>
+        <>
+            <Navagation />
+            <div className="containerResume">
+                <div className="resume_section_1"></div>
+                <div className="resume_section_2"></div>
+                <div className="resume_section_3"></div>
+                <div className="resume_section_4"></div>
+                <div className="resume_section_5"></div>
+                <div className="resume_section_6"></div>
+                <div className="resume_section_7">
+
+                </div>
+                <div className="resume_section_8"></div>
+                <div className="resume_section_9"></div>
+                <div className="resume_section_10"></div>
+                <section className="resume_section_11">
+                    <article>
+                        <h3 className="resume_title_type">SOFTWARE DEVELOPER • FREELANCE </h3>
+                        <h4 className="resume_section_type">Portland, OR • 2020 – Present</h4>
+                        <p className="resume_paragraph_type">Recruited to build features for a website for recently physically disabled people. Developed using React, Material UI and the latest React testing library.</p>
+                        <ul className="resume_list_type">
+                            <li>Built post and comment components for the site in React; streamlined code reviews by implementing GitHub continuous integration on all development and production repositories.</li>
+                            <li>Instrumental in planning, researching, and teaching core W3C principles of accessibility design to volunteer developers.</li>
+                            <li>Created project-specific accessibility guidance documents for the development team and led introductory accessibility design sessions for volunteers.</li>
+                            <li>Recruited and onboarded three volunteer developers to the project team over two weeks.</li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h3 className="resume_title_type"> SOFTWARE DEVELOPER TEACHING ASSISTANT • ALCHEMY CODE LAB </h3>
+                        <h4 className="resume_section_type">Portland, OR • 2020 – 2021 </h4>
+                        <p className="resume_paragraph_type">Under the supervision of the lead instructor, taught JavaScript Node.js, React, and PostgreSQL to a group of five students during a six-month software development bootcamp.</p>
+                        <ul className="resume_list_type">
+
+                            <li>Graded, debugged, and provided feedback on Node.js, React, and SQL lessons; completed code review for student projects through GitHub pull requests.</li>
+                            <li>Refined the instruction process to assist with online-only classes by utilizing smaller TA groups in the first week to mirror an in-person table discussion.</li>
+                            <li>Taught PostgreSQL with one-week lead time by analyzing documentation, building small projects, and sandboxing code.</li>
+                            <li>Developed documentation structure to detail and archive all Alchemy projects from 2019 to the present.</li>
+                        </ul>
+                    </article>
+                    <article>
+                        <h3 className="resume_title_type"> COMPUTER SCIENCE INTERN • PORTLAND STATE COMPUTER ACTION TEAM </h3>
+                        <h4 className="resume_section_type">Portland, OR • 2016 – 2017 </h4>
+                        <p className="resume_paragraph_type">Maintained computer systems in the Computer Science and Engineering Department at Portland State University</p>
+                        <ul className="resume_list_type">
+                            <li>Trained and advised users during computer setup and campus network onboarding.</li>
+                            <li>Updated and maintained Red Hat servers and Ubuntu computer labs.</li>
+                        </ul>
+                    </article>
+
+                </section>
+                <div className="resume_section_12"></div>
+                <div className="resume_section_13"></div>
+                <div className="resume_section_14"></div>
+                <div className="resume_section_15"></div>
+                <div className="resume_section_16"></div>
+            </div>
+        </>
     )
 }
--- src/TechnicalSkill/TechnicalSkill.jsx
+++ src/TechnicalSkill/TechnicalSkill.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import Navagation from '../Navagation/Navagation'
+import './TechicalSkill.css'
 
 export default function TechnicalSkill() {
     return (
-        <div>
-            hay
-        </div>
+        <>
+            <Navagation />
+            <div className="containerWriting">
+                <section className="learning_title">
+                    <h1 className="learning_title_text">
+                        TechnicalSkill
+                    </h1>
+                </section>
+                <article className="technical_article">
+                    <ul className="technical_paragraph">
+                        <li><p className="technical_paragraph">Javascript </p></li>
+                        <li><p className="technical_paragraph">CSS </p></li>
+                        <li><p className="technical_paragraph">React </p></li>
+                        <li> <p className="technical_paragraph">HTML </p></li>
+                        <li> <p className="technical_paragraph">SQL </p></li>
+                        <li> <p className="technical_paragraph">Jest</p></li>
+                        <li><p className="technical_paragraph">React </p></li>
+                        <li> <p className="technical_paragraph">Express  </p></li>
+                        <li> <p className="technical_paragraph">Node.js </p></li>
+                        <li> <p className="technical_paragraph">Next.js</p></li>
+                    </ul>
+                </article>
+                <div className="technical_section_1"></div>
+                <div className="technical_section_2"></div>
+                <div className="technical_section_3"></div>
+                <div className="technical_section_4"></div>
+                <div className="technical_section__5"></div>
+                <div className="technical_section__6"></div>
+                <div className="technical_section__7"></div>
+                <div className="technical_section__8"></div>
+                <div className="technical_section__9"></div>
+                <div className="technical_section__10"></div>
+                <div className="technical_section__11"></div>
+                <div className="technical_section__12"></div>
+                <div className="technical_section__13"></div>
+                <div className="technical_section__14"></div>
+                <div className="technical_section__15"></div>
+
+            </div>
+        </>
     )
+
 }
